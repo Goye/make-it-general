8e3d57e20039efd5bf3fa7a6a41534ef252c414c
Goye <karlos.goye@gmail.com>
2020-09-01 19:19:48 -0500

    revert changes

--- user.js
+++ user.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require("bcrypt");
 
 const schema = mongoose.Schema({
     name: { type: String, required: true },
@@ -7,14 +6,4 @@ const schema = mongoose.Schema({
     password: { type: String, required: true }
 });
 
-// hashes the password
-schema.pre("save", function(next) {
-    bcrypt.hash(this.password, 10, (err, hash) => {
-        if (err) { return next(err); }
-
-        this.password = hash;
-        next();
-    });
-});
-
-module.exports = mongoose.model("User", schema)
+module.exports = mongoose.model("Visitor", schema)
