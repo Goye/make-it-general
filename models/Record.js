8304f6ed8a2f4a90ec02a62c4745d51f938eeb8a
Carlos Goyeneche <karlos.goye92@hotmail.com>
2023-03-13 14:42:03 -0500

    Advanced patterns exercise

--- models/Record.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const RecordSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
-});
-const Record = mongoose.model("Record", RecordSchema);
-module.exports = Record
